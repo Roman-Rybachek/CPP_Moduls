3f9c5829ec34e35776774ae0cae6a95ae29c08fb
Janelle Eldora <jeldora@oa-d3.msk.21-school.ru>
2020-10-22 22:55:19 +0300

    пишу второе задание

---
+++ module_00/ex01/ContactClass.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ContactClass.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeldora <jeldora@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/22 21:11:12 by jeldora           #+#    #+#             */
+/*   Updated: 2020/10/22 22:22:40 by jeldora          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+class Contact
+{
+	std::string first_name;
+	std::string last_name;
+	std::string nickname;
+	std::string login;
+	std::string postal_address;
+	std::string email_address;
+	std::string phone_number;
+	std::string birthdat_date;
+	std::string favorite_meal;
+	std::string underwear_color;
+	std::string darkest_secret;
+};
---
+++ module_00/ex01/phonebook.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeldora <jeldora@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/22 22:10:43 by jeldora           #+#    #+#             */
+/*   Updated: 2020/10/22 22:53:26 by jeldora          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "phonebook.hpp"
+
+int		main()
+{
+	std::string command;
+	Contact list[8];
+
+	std::cout << "Hello. It is a phonebook.\n";
+	while(true)
+	{
+		std::cout << "The available commands: ADD, SEARCH, EXIT\n";
+		std::cin >> command;
+		if (command == "ADD")
+			add();
+		else if (command == "SEARCH")
+			search();
+		else if (command == "EXIT")
+			just_exit();
+		else
+			continue ;
+	}
+	return (0);
+}
